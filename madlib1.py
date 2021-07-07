b2cfd6efefaa12f65fdd0236ba627fbf761d8817
sumanth-26 <87059288+sumanth-26@users.noreply.github.com>
2021-07-07 12:33:59 +0530

    Add files via upload

---
+++ madlib1.py
@@ -0,0 +1,6 @@
+name = input("Enter your name:")
+age = input("Enter your age:")
+degree = input("Enter your degree name:")
+college = input("Enter your college name:")
+mad_lib = f"I'm {name}.My age is {age}.I am pursuing {degree} at {college}"
+print(mad_lib)
