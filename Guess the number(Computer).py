e999e824a1ebe25a40446fcfe07ed7c06ce9b72c
sumanth-26 <87059288+sumanth-26@users.noreply.github.com>
2021-07-07 13:04:10 +0530

    Add files via upload

---
+++ Guess the number(Computer).py
@@ -0,0 +1,12 @@
+import random
+def guess(x):
+    random_number = random.randint(1,x)
+    guess = 0
+    while guess!=random_number:
+        guess = int(input(f"Guess a number between 1 and {x}:"))
+        if guess < random_number:
+            print("Too low")
+        elif guess > random_number:
+            print("Too high")
+    print("You have guessed the correct number")
+guess(10)
